c1f78809663a1110f172a93c85766094bed3db25
abrarnabil <abrarnabil574@gmail.com>
2025-10-21 12:15:35 +0300

    update

--- RoseCafe/src/components/home/Header.jsx
+++ RoseCafe/src/components/home/Header.jsx
@@ -37,10 +37,10 @@ function Header() {
         </div>
         <ul className={`nav__links ${isMenuOpen ? "open" : ""}`} id="nav-links">
       <li><Link to="/">Home</Link></li>
-            <li><Link to="/menu">Menu</Link></li>
+            <li><Link to="/menu">Menukjhj</Link></li>
             <li><Link to="/about">About Us</Link></li>    
             <li><Link to="/login"> login </Link></li>
-<li><Link to="/register"> registerr </Link></li>
+<li><Link to="/register"> register </Link></li>
 
 
 
